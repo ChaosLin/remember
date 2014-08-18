b3e145aaf1871a1f7fbee379f29f6025c123620c
RentonTheUncoped <RentonTheUncoped@Danteng-2.local>
2014-08-18 09:54:12 +0800

    Initial Commit

---
+++ remember/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  remember
+//
+//  Created by RentonTheUncoped on 14-8-18.
+//  Copyright (c) 2014年 Uncoped Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
